931c301cc9d6912e0feac74f9c48e4f4ff46e603
Prashant Kumar <programmerkrprashant@gmail.com>
2024-08-30 22:39:52 +0530

    Responsive

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,6 +4,7 @@ import Layout from './Layout'
 import { CreateTask } from './components/CreateTask'
 import { Task } from './components/Task'
 import { Home } from './components/Home'
+import MobileSearch from './components/MobileSearch'
 
 const Root = () => {
     return (
@@ -13,7 +14,9 @@ const Root = () => {
                     <Route path='createTask' element={<CreateTask />} />
                     <Route path='allTask' element={<Task />} />
                     <Route path='/' element={<Home />} />
+                    
                 </Route>
+                <Route path='/Search' element={<MobileSearch />} />
             </Routes>
         </BrowserRouter>
     )
