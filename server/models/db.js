8487cbee859dc6040533b3d4b7cdf9bc0cfd9046
himansu naik <naikhimansu546@gmail.com>
2024-10-07 19:47:51 +0530

    resolved missing connectionString

--- server/models/db.js
+++ server/models/db.js
@@ -11,11 +11,19 @@ const knex = require("knex");
 //   },
 // });
 
+const knex = require("knex");
+
 const db = knex({
   client: "pg",
-  connection:
-    "postgresql://profilestore_user:seyPY5L6v8tTPkRlIiN1N864qnvExs8c@dpg-cs1tamlds78s73bagit0-a.oregon-postgres.render.com/profilestore",
-  ssl: { rejectUnauthorized: true },
+  connection: {
+    connectionString:
+      "postgresql://profilestore_user:seyPY5L6v8tTPkRlIiN1N864qnvExs8c@dpg-cs1tamlds78s73bagit0-a.oregon-postgres.render.com/profilestore",
+    ssl: {
+      rejectUnauthorized: true, // Change to true for production
+    },
+  },
 });
+
+module.exports = db;
 
 module.exports = db;
