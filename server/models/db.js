5c0732414c64e8e4e64985131278f78221825084
himansu naik <naikhimansu546@gmail.com>
2024-10-07 21:57:30 +0530

    updated

--- server/models/db.js
+++ server/models/db.js
@@ -13,17 +13,30 @@ const knex = require("knex");
 
 // const knex = require("knex");
 
+// const db = knex({
+//   client: "pg",
+//   connection: {
+//     connectionString:
+//       "postgresql://profilestore_user:seyPY5L6v8tTPkRlIiN1N864qnvExs8c@dpg-cs1tamlds78s73bagit0-a.oregon-postgres.render.com/profilestore",
+//     ssl: {
+//       rejectUnauthorized: true, // Change to true for production
+//     },
+//   },
+// });
+
 const db = knex({
   client: "pg",
   connection: {
-    connectionString:
-      "postgresql://profilestore_user:seyPY5L6v8tTPkRlIiN1N864qnvExs8c@dpg-cs1tamlds78s73bagit0-a.oregon-postgres.render.com/profilestore",
+    host: "dpg-cs1tamlds78s73bagit0-a.oregon-postgres.render.com",
+    user: "profilestore_user",
+    password: "seyPY5L6v8tTPkRlIiN1N864qnvExs8c",
+    database: "profilestore",
     ssl: {
-      rejectUnauthorized: true, // Change to true for production
+      rejectUnauthorized: true, // Use true for production
     },
   },
 });
 
-module.exports = db;
+// module.exports = db;
 
 module.exports = db;
